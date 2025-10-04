e15aedea4d2edc64ab2675cb3f4f836354f25819
GyuoMi <darshnsingh10@gmail.com>
2025-10-04 17:59:08 +0200

    2-5 for part a and mostly complete part b

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	g++ -std=c++11 -o part_a part_a.cpp && ./part_a;
+	g++ -std=c++11 -o part_b part_b.cpp && ./part_b;
--- part_b.cpp
+++ part_b.cpp
@@ -16,6 +16,7 @@ struct Node
     Node *parent;
     Node *left;
     Node *right;
+    int size;
 
     Node(int k)
     {
@@ -23,6 +24,7 @@ struct Node
         parent = nullptr;
         left = nullptr;
         right = nullptr;
+        size = 1;
     }
 };
 
@@ -34,6 +36,8 @@ void treeInsert(Node *&root, Node *z)
     while (x != nullptr)
     {
         y = x;
+        // increment size of nodes as we traverse path
+        y->size++;
         if (z->key < x->key)
         {
             x = x->left;
@@ -98,27 +102,54 @@ void freeTree(Node *node){
     freeTree(node->right);
     delete node;
 }
+
+// decrement ancestors for treeDelete
+void decrementAncestors(Node *node){
+    while (node != nullptr){
+        node->size--;
+        node = node->parent;
+    }
+}
 //  end of subroutines/helpers
 
 void treeDelete(Node *&root, Node *z)
 {
+    if (z == nullptr) return;
+
+    Node *node_to_update = nullptr;
+
     if (z->left == nullptr){
+        // case where z has no left child
+        node_to_update = z->parent;
         transplant(root, z, z->right);
     }
     else if (z->right == nullptr){
+        // case where z has a left child but no right child
+        node_to_update = z->parent;
         transplant(root, z, z->left);
     }
     else {
+        // case where z has two children
         Node* y = treeMinimum(z->right);
+        // y is not z's direct child, y will be removed
         if (y->parent != z){
+            node_to_update = y->parent;
             transplant(root, y, y->right);
             y->right = z->right;
             y->right->parent = y;
         }
+        else{
+            // if y is z's direct child, y should replace z
+            node_to_update = y;
+        }
         transplant(root, z, y);
         y->left = z->left;
         y->left->parent = y;
+
+        // update y's size based on it's new children
+        y->size = (y->left ? y->left->size : 0) + (y->right ? y->right->size : 0) + 1;
     }
+    decrementAncestors(node_to_update);
 }
 
 void inOrderTreeWalk(Node *x){
@@ -129,14 +160,63 @@ void inOrderTreeWalk(Node *x){
     }
 }
 
+Node* OS_SELECT(Node *x, int i){
+    if (x == nullptr || i <= 0){
+        return nullptr;
+    }
+    
+    // so we do this check here since the normal RB tree pseudocode does the following
+    // it checks the left child of x's size without checking if it is a nil i.e. nullptr
+    // accessing mem that doesnt exist thus segfaulting
+    int left_size;
+    if (x->left != nullptr){
+        left_size = x->left->size;
+    }
+    else {
+        left_size = 0;
+    }
+    int r = left_size + 1;
+
+    if (i == r){
+        return x;
+    }
+    else if (i < r){
+        return OS_SELECT(x->left, i);
+    }
+    else return OS_SELECT(x->right, i - r);
+}
+
+int OS_RANK(Node *root, Node *x){
+    if (x == nullptr){
+        return 0;
+    }
+
+    int left_size;
+    if (x->left != nullptr){
+        left_size = x->left->size;
+    }
+    else {
+        left_size = 0;
+    }
+    int r = left_size + 1;
+
+    Node *y = x;
+    while (y != root){
+        if (y == y->parent->right){
+            r = r + y->parent->left->size + 1;
+        }
+        y = y->parent;
+    }
+    return r;
+}
+
 int main(){
     srand(time(0));
 
     std::ofstream outFile;
-    outFile.open("bst_results.csv");
+    outFile.open("ost_results.csv");
 
-    outFile << "n,ExpectedHeight,AvgBuildTime_ms,AvgDestroyTime_ms,AvgWalkTime_ms\n";
-
+    outFile << "n,AvgBuildTime_ms,AvgDestroyTime_ms,AvgSelectTime_ms,AvgRankTime_ms\n";
     int n_vals[] = {100, 500, 1000, 2000, 5000, 10000} ;
     int n_trials = 50;
 
@@ -147,6 +227,8 @@ int main(){
         double total_build_time = 0;
         double total_destroy_time = 0;
         double total_walk_time = 0;
+        double total_select_time = 0;
+        double total_rank_time = 0;
 
         for (int j = 0; j < n_trials; ++j){
             Node *root = nullptr;
@@ -177,10 +259,21 @@ int main(){
 
             total_height += treeHeight(root);
 
-            clock_t start_walk = clock();
-            inOrderTreeWalk(root);
-            clock_t end_walk = clock();
-            total_walk_time += (double)(end_walk - start_walk) * 1000.0 / CLOCKS_PER_SEC;
+            // clock_t start_walk = clock();
+            // inOrderTreeWalk(root);
+            // clock_t end_walk = clock();
+            // total_walk_time += (double)(end_walk - start_walk) * 1000.0 / CLOCKS_PER_SEC;
+
+            clock_t start_select = clock();
+            OS_SELECT(root, n / 2);
+            clock_t end_select = clock();
+            total_select_time += (double)(end_select - start_select) * 1000.0 / CLOCKS_PER_SEC;
+
+            clock_t start_rank = clock();
+            OS_RANK(root, root); 
+            clock_t end_rank = clock();
+            total_rank_time += (double)(end_rank - start_rank) * 1000.0 / CLOCKS_PER_SEC;
+
 
             clock_t start_destroy = clock();
             while (root != nullptr){
@@ -194,13 +287,13 @@ int main(){
             delete[] keys;
         }
 
-        double expected_height = total_height / n_trials;
         double avg_build_time = total_build_time / n_trials;
         double avg_destroy_time = total_destroy_time / n_trials;
-        double avg_walk_time = total_walk_time / n_trials;
+        double avg_select_time = total_select_time / n_trials;
+        double avg_rank_time = total_rank_time / n_trials;
 
-        outFile << n << "," << expected_height << "," << avg_build_time << "," << avg_destroy_time 
-        << "," << avg_walk_time << std::endl;
+        outFile << n <<  "," << avg_build_time << "," << avg_destroy_time 
+        << "," << avg_select_time << "," << avg_rank_time << std::endl;
     }
     outFile.close();
     return 0;
